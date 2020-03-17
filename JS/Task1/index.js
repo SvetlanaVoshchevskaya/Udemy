e12cda6291cf6147c5de7842053e904eef016ac4
Svetlana <VSveta.83@gmail.com>
2020-03-17 20:10:56 +0200

    add new function

--- JS/Task1/index.js
+++ JS/Task1/index.js
@@ -8,62 +8,81 @@ const appDate = {
   expenses: {},
   optinalExpenses: {},
   income: [],
-  saving: true
-};
-
-function askAnswer() {
-  money = +prompt('Your budget at month?', '');
-  time = prompt('Enter the date as YYYY-MM-DD', '');
-
-  while (isNaN(money) || money === '' || money === null) {
+  saving: true,
+  askAnswer() {
     money = +prompt('Your budget at month?', '');
-  }
-  appDate.budget = money;
-  appDate.timeDate = time;
-}
-askAnswer();
+    time = prompt('Enter the date as YYYY-MM-DD', '');
 
-function chooseExpenses() {
-  for (let i = 0; i < 2; i++) {
-    let monthExpenses = prompt('Enter obligatory expenses at this month', '');
-    let summExpenses = +prompt('Enter the summ', '');
+    while (isNaN(money) || money === '' || money === null) {
+      money = +prompt('Your budget at month?', '');
+    }
+    appDate.budget = money;
+    appDate.timeDate = time;
+  },
+  chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+      let monthExpenses = prompt('Enter obligatory expenses at this month', '');
+      let summExpenses = +prompt('Enter the summ', '');
 
+      if (
+        monthExpenses !== '' &&
+        summExpenses !== '' &&
+        monthExpenses !== null &&
+        summExpenses !== null
+      ) {
+        appDate.expenses[monthExpenses] = summExpenses;
+      } else {
+        i--;
+      }
+    }
+  },
+  detectDayBudget() {
+    appDate.moneyPerDay = (appDate.budget / days).toFixed();
+    alert('Бюджет на день ' + appDate.moneyPerDay);
+  },
+  saving() {
+    if (appDate.saving) {
+      let summSaving = +prompt('How much saving', '');
+      let percent = +prompt('How much percent', '');
+      appDate.monthIncoming = (summSaving / 100 / 12) * percent;
+      alert(appDate.monthIncoming.toFixed());
+    }
+  },
+  chooseOptExpenses() {
+    appDate.optinalExpenses = {};
+    for (let i = 1; i < 3; i++) {
+      let optinalExpenses = prompt('Статья необязательных расходов?', '');
+      appDate.optinalExpenses[i] = optinalExpenses;
+    }
+  },
+  chooseIncome() {
+    let incomeItem = prompt(
+      'Что приносит дополнительный доход? (перечислите через запятую)',
+      ''
+    );
     if (
-      monthExpenses !== '' &&
-      summExpenses !== '' &&
-      monthExpenses !== null &&
-      summExpenses !== null
-    ) {
-      appDate.expenses[monthExpenses] = summExpenses;
-    } else {
-      i--;
+      typeof incomeItem !== 'string' ||
+      incomeItem === '' ||
+      incomeItem === null
+    )
+      return;
+    appDate.income = incomeItem.split(', ');
+    appDate.income.push(prompt(' Что то еще?', ''));
+    appDate.income.sort();
+    appDate.income.forEach((el, i, arr) =>
+      alert('Способы доп. заработка: ' + arr)
+    );
+    for (let el in appDate) {
+      console.log('Наша программа включает в себя данные: ' + el);
     }
   }
-}
-chooseExpenses();
+};
 
-function detectDayBudget() {
-  appDate.moneyPerDay = (appDate.budget / days).toFixed();
-  alert('Бюджет на день ' + appDate.moneyPerDay);
-}
+appDate.askAnswer();
+appDate.chooseExpenses();
 
-function saving() {
-  if (appDate.saving) {
-    let summSaving = +prompt('How much saving', '');
-    let percent = +prompt('How much percent', '');
-    appDate.monthIncoming = (summSaving / 100 / 12) * percent;
-    alert(appDate.monthIncoming.toFixed());
-  }
-}
-
-function chooseOptExpenses() {
-  appDate.optinalExpenses = {};
-  for (let i = 0; i < 3; i++) {
-    let optinalExpenses = prompt('Статья необязательных расходов?', '');
-    appDate.optinalExpenses[i + 1] = optinalExpenses;
-  }
-}
-detectDayBudget();
-saving();
-chooseOptExpenses();
+appDate.detectDayBudget();
+appDate.saving();
+appDate.chooseOptExpenses();
+appDate.chooseIncome();
 console.log(appDate);
